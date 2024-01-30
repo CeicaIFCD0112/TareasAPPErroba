44ff4c2383b57c8b6d4cb29b20a953bfc9407198
CeicaIFCD0112 <156108500+CeicaIFCD0112@users.noreply.github.com>
2024-01-30 19:27:45 +0100

    Delete src/com/ceica directory

--- src/com/ceica/Controladores/LoginController.java
+++
@@ -1,9 +0,0 @@
-package com.ceica.Controladores;
-
-import com.ceica.modelos.Usuario;
-
-public class LoginController {
-    public static boolean login(String usuario,String password){
-        return Usuario.getUsuario(usuario,password);
-    }
-}
